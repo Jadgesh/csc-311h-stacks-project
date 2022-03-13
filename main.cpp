d1f4692ab8bd05487960ba6f5aee274fd79c387c
Jadgesh <jadgesh.i@gmail.com>
2022-03-13 06:25:39 +0000

    test

--- main.cpp
+++ main.cpp
@@ -2,25 +2,7 @@
 #include "stack.cpp"
 
 int main(){
-    Stack<int> stx;
-    
-    for(int i = 0; i < 10; i++){
-        stx.push(i);
-    }
-    
-    std::cout << "Is stack empty? ";
-    if(stx.isEmpty())
-        std::cout << "TRUE\n";
-    else
-        std::cout << "FALSE\n";
-    
-    std::cout << "Stack content: ";
-    
-    while(!stx.isEmpty()){
-        std::cout << stx.top() << " ";
-        stx.pop();
-    }
-    
-    std::cout << "It works!\n";
+    Stack<double> operands;
+    Stack<char> operators;
     return 0;
 }
